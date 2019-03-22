dd07b00a1116086374adccd54c19523bca9f1941
Teh Kok How <funcoolgeek@gmail.com>
2019-03-22 10:17:55 +0800

    Use existing access-log-template.json

--- iconverse/elasticsearch_access_logs_init.sh
+++ iconverse/elasticsearch_access_logs_init.sh
@@ -3,5 +3,5 @@
 host=svc-elasticsearch:9200
 curl -XDELETE http://$host/iconverse.access.logs* --retry 10
 echo
-curl -XPUT -H "Content-Type: application/json" --data @resources/access_logs_template.json http://$host/_template/access-log-template --retry 10
+curl -XPUT -H "Content-Type: application/json" --data @/tmp/access_log_template.json http://$host/_template/access-log-template --retry 10
 echo
