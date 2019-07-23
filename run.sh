a4837136a33862cf419b2a072cd4444390571a65
Teh Kok How <khteh@KHTeh-T490.biz4x.com>
2019-07-23 15:13:40 +0800

    Add biz4x indices

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@ set -x
 host=svc-elasticsearch:9200
 curl -XDELETE http://$host/aspnetapistarter.logs-* --retry 10
 echo
+curl -XDELETE http://$host/biz4x-backend.logs-* --retry 10
+echo
+curl -XDELETE http://$host/biz4x-frontend.logs-* --retry 10
+echo
 curl -XPUT -H "Content-Type: application/json" --data @/tmp/access_log_template.json http://$host/_template/access-log-template --retry 10
 echo
