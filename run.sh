a669b236b9c7bd1c2adc95d339946c2bd92d1ff2
Kok How, Teh <funcoolgeek@gmail.com>
2022-12-13 16:01:51 +0800

    Update ECK endpoint

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -x
 #ulimit -l unlimited
-#host=kyberlife-es-default:9200
-host=svc-elasticsearch:9200
-curl -s -XDELETE http://$host/kyberlifeiam.logs-* --retry 10
+host=kyberlife-es-http:9200
+curl -XDELETE http://$host/kyberlifeiam.logs-* --retry 10
 echo
-curl -s -XDELETE http://$host/kyberlifeproduct.logs-* --retry 10
+curl -XDELETE http://$host/kyberlifeproduct.logs-* --retry 10
 echo
-curl -s -XPUT -H "Content-Type: application/json" --data @/tmp/access_log_template.json -u $ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD -k https://$host/_template/access-log-template --retry 10
+curl -XPUT -H "Content-Type: application/json" --data @/tmp/access_log_template.json -u elastic:$ELASTICSEARCH_PASSWORD -k https://$host/_template/access-log-template --retry 10
 echo
